228e748121fb58bb55f65766d3b326feb5943630
Malte Niepel <maltetorben.niepel@gmail.com>
2020-08-17 08:18:08 +0200

    added etfs to the scraper

--- us_bonds_analysis/scraper/scraper.py
+++ us_bonds_analysis/scraper/scraper.py
@@ -6,6 +6,8 @@ def get_etf_prices():
     etfs = [
         "https://www.ishares.com/us/products/239566/ishares-iboxx-investment-grade-corporate-bond-etf",
         "https://www.ishares.com/us/products/239468/ishares-us-treasury-bond-etf",
+        "https://www.ishares.com/us/products/239655/ishares-msci-global-metals-mining-producers-etf",
+        "https://www.ishares.com/us/products/239561/ishares-gold-trust-fund",
     ]
 
     etf_prices = {}
@@ -29,3 +31,5 @@ def get_etf_prices():
         etf_prices[name] = float(price)
 
     return etf_prices
+
+print(get_etf_prices())
